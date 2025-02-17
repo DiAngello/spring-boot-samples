8085a7ff0aa92a7befd79d488cf46b439980b858
Andressa <andressa.gabrielly.ofc@gmail.com>
2025-02-17 13:24:14 -0300

    Aula 11

--- JAVA/src/Variaveis.java
+++ JAVA/src/Variaveis.java
@@ -1,7 +1,7 @@
 public class Variaveis {
 
         public static void main(String[] args) {
-            //Tipos primitivos//
+            //Variaveis
             int idade;
             String nome = "Andressa";
             String nomeDoMeuCachorro = "Bob";
@@ -11,5 +11,14 @@ public class Variaveis {
             System.out.println("Idade = "+idade);
             System.out.println("Nome = "+nome);
             System.out.println("Nome do cachorro = "+nomeDoMeuCachorro);
+
+            char letraA = 97;
+
+            double valorDaPassagem01 = 5.50;
+            float valorDaPassagem02 = 3.80F;
+
+            boolean verdadeiro = true;
+            
+            System.out.println("Char = "+ letraA);
         }
 }
