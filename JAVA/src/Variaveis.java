3dede145d95434b4b0f47c8864ea7d79019f07b8
Andressa <andressa.gabrielly.ofc@gmail.com>
2025-02-17 12:41:54 -0300

    Aula 10

---
+++ JAVA/src/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+
+        public static void main(String[] args) {
+            //Tipos primitivos//
+            int idade;
+            String nome = "Andressa";
+            String nomeDoMeuCachorro = "Bob";
+
+            idade = 23;
+
+            System.out.println("Idade = "+idade);
+            System.out.println("Nome = "+nome);
+            System.out.println("Nome do cachorro = "+nomeDoMeuCachorro);
+        }
+}
