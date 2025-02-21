dcc510881ffa1fc3af68bb5ba0e2ce79f28f3588
Andressa <andressa.gabrielly.ofc@gmail.com>
2025-02-21 20:44:12 -0300

    Base webapp Spring Boot

---
+++ basewebapp-spring-boot/src/test/java/com/curso/basewebapp_spring_boot/BasewebappSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.basewebapp_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasewebappSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
